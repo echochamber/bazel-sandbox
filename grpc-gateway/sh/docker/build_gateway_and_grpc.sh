7153629c479c821491eb7963c21fee124e59d043
Jason Schein <jasonschein@gmail.com>
2023-05-15 19:39:10 -0700

    Update docker grpc gateway

--- grpc-gateway/sh/docker/build_gateway_and_grpc.sh
+++ grpc-gateway/sh/docker/build_gateway_and_grpc.sh
@@ -1,4 +1,4 @@
 git_root="$(git rev-parse --show-toplevel)"
 
-docker build -t grpc-gateway  "${git_root}/gen/grpc-gateway"
+docker build -t grpc-gateway  "${git_root}/grpc-gateway/docker-gen"
 docker build -t grpc "${git_root}/grpc"
