08393e7244da20c2f3aa9118bce6eb5b874ef80c
Jason Schein <jasonschein@gmail.com>
2023-05-16 17:18:43 -0700

    External proto defs are now vendored using `buf` tool.

--- grpc/src/main.rs
+++ grpc/src/main.rs
@@ -1,25 +1,23 @@
 use tonic::{transport::Server, Request, Response, Status};
 
-use echochamber_rdproto::echochamber::helloworld::v1::{Greeter, GreeterServer};
-use echochamber_rdproto::echochamber::helloworld::v1::{SayHelloResponse, SayHelloResponse};
+use echochamber_rdproto::echochamber::helloworld::v1::greeter_service_server::{GreeterServiceServer, GreeterService};
+use echochamber_rdproto::echochamber::helloworld::v1::{SayHelloRequest, SayHelloResponse};
 
-pub mod hello_world {
-    tonic::include_proto!("echochamber.helloworld.v1"); // The string specified here must match the proto package name
-}
+
 
 #[derive(Debug, Default)]
 pub struct MyGreeter {}
 
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
+impl GreeterService for MyGreeter {
     async fn say_hello(
         &self,
-        request: Request<HelloRequest>, // Accept request of type HelloRequest
-    ) -> Result<Response<HelloReply>, Status> { // Return an instance of type HelloReply
+        request: Request<SayHelloRequest>, // Accept request of type HelloRequest
+    ) -> Result<Response<SayHelloResponse>, Status> { // Return an instance of type HelloReply
         println!("Got a request: {:?}", request);
 
-        let reply = hello_world::HelloReply {
+        let reply = SayHelloResponse {
             message: format!("Hello {}!", request.into_inner().name).into(), // We must use .into_inner() as the fields of gRPC requests and responses are private
         };
 
@@ -30,16 +28,17 @@ impl Greeter for MyGreeter {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "0.0.0.0:50052".parse()?;
+    let addr = "0.0.0.0:50051".parse()?;
     let greeter = MyGreeter::default();
 
     let reflection = tonic_reflection::server::Builder::configure()
-        .register_encoded_file_descriptor_set(tonic::include_file_descriptor_set!("descriptor"))
+        .register_encoded_file_descriptor_set(echochamber_rdproto::echochamber::helloworld::v1::FILE_DESCRIPTOR_SET)
+        .register_encoded_file_descriptor_set(echochamber_rdproto::google::api::FILE_DESCRIPTOR_SET)
         .build()
         .unwrap();
 
     Server::builder()
-        .add_service(GreeterServer::new(greeter))
+        .add_service(GreeterServiceServer::new(greeter))
         .add_service(reflection)
         .serve(addr)
         .await?;
